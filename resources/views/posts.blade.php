6e0e078b3581d03a4d84dcf1fab4994dd28da553
HadertS <source.zero@googlemail.com>
2023-11-13 16:12:33 +0000

    End of Day 1 Commit - Episode 7 03:30

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+<article>
+    <h1>My First Post</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at molestie tellus. Sed pulvinar consequat ante id sollicitudin. Maecenas at maximus nulla, ac commodo felis. Integer vitae neque nec ligula iaculis rutrum sed non nisi. Vivamus purus nunc, sagittis ut efficitur id, pellentesque vitae lorem. Mauris mollis vehicula ipsum in sodales. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean mollis feugiat eros. Nunc a sem congue, tristique nibh in, facilisis nisi. Aliquam efficitur justo quis sollicitudin dignissim. Sed sit amet quam nibh. Pellentesque semper massa dolor, quis eleifend nisl finibus quis. Pellentesque interdum elit at libero tincidunt tincidunt. Mauris semper sagittis nulla, sit amet tincidunt diam viverra eu.
+    </p>
+</article>
+<article>
+    <h1>Second Post</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at molestie tellus. Sed pulvinar consequat ante id sollicitudin. Maecenas at maximus nulla, ac commodo felis. Integer vitae neque nec ligula iaculis rutrum sed non nisi. Vivamus purus nunc, sagittis ut efficitur id, pellentesque vitae lorem. Mauris mollis vehicula ipsum in sodales. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean mollis feugiat eros. Nunc a sem congue, tristique nibh in, facilisis nisi. Aliquam efficitur justo quis sollicitudin dignissim. Sed sit amet quam nibh. Pellentesque semper massa dolor, quis eleifend nisl finibus quis. Pellentesque interdum elit at libero tincidunt tincidunt. Mauris semper sagittis nulla, sit amet tincidunt diam viverra eu.
+    </p>
+</article>
+<article>
+    <h1>Third Post</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at molestie tellus. Sed pulvinar consequat ante id sollicitudin. Maecenas at maximus nulla, ac commodo felis. Integer vitae neque nec ligula iaculis rutrum sed non nisi. Vivamus purus nunc, sagittis ut efficitur id, pellentesque vitae lorem. Mauris mollis vehicula ipsum in sodales. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean mollis feugiat eros. Nunc a sem congue, tristique nibh in, facilisis nisi. Aliquam efficitur justo quis sollicitudin dignissim. Sed sit amet quam nibh. Pellentesque semper massa dolor, quis eleifend nisl finibus quis. Pellentesque interdum elit at libero tincidunt tincidunt. Mauris semper sagittis nulla, sit amet tincidunt diam viverra eu.
+    </p>
+</article>
+</body>
