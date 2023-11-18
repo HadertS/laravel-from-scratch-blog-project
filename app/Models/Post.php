5d7c8bea312b053c1841fd242495612a9f05cbc4
HadertS <source.zero@googlemail.com>
2023-11-18 19:52:29 +0000

    End of Day 5 Commit - Episode 21 00:00

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,52 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public string $title;
-    public string $slug;
-    public string $excerpt;
-    public string $date;
-    public string $body;
-
-    /**
-     * @param string $title
-     * @param string $slug
-     * @param string $excerpt
-     * @param string $date
-     * @param string $body
-     */
-    public function __construct(string $title, string $slug, string $excerpt, string $date, string $body)
-    {
-        $this->title = $title;
-        $this->slug = $slug;
-        $this->excerpt = $excerpt;
-        $this->date = $date;
-        $this->body = $body;
-    }
-
-    public static function all()
-    {
-        return cache()->rememberForever('posts.all',function(){
-        return $posts = collect(File::files(resource_path("posts")))
-            ->map(fn($file) => YamlFrontMatter::parseFile($file))
-            ->map(fn($document) => new Post(
-                $document->title,
-                $document->slug,
-                $document->excerpt,
-                $document->date,
-                $document->body()
-            ))
-            ->sortByDesc('date');
-        });
-    }
-    public static function find($slug)
-    {
-        return static::all()->firstWhere('slug',$slug);
-    }
-
+    use HasFactory;
 }
